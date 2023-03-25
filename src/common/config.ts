028d133afdcf8482c4981d7d23e68ad7e9a4cfbf
Alvin <alvin.lm.liu@gmail.com>
2023-03-25 18:41:47 +0800

    feat: add platform choices

--- src/common/config.ts
+++ src/common/config.ts
@@ -4,3 +4,62 @@ export const templateConfig = [
     value: 'vite-vue3-ts'
   },
 ];
+
+export const platformConfig = [
+  {
+    name: 'app',
+    value: 'app'
+  },
+  {
+    name: 'h5',
+    value: 'h5'
+  },
+  {
+    name: '微信小程序',
+    value: 'mp-weixin'
+  },
+  {
+    name: '支付宝小程序',
+    value: 'mp-alipay'
+  },
+  {
+    name: '百度小程序',
+    value: 'mp-baidu'
+  },
+  {
+    name: '字节跳动小程序',
+    value: 'mp-toutiao'
+  },
+  {
+    name: 'QQ小程序',
+    value: 'mp-qq'
+  },
+  {
+    name: '360小程序',
+    value: 'mp-360'
+  },
+  {
+    name: '快手小程序',
+    value: 'mp-kuaishou'
+  },
+  {
+    name: '飞书小程序',
+    value: 'mp-lark'
+  },
+  {
+    name: '京东小程序',
+    value: 'mp-jd'
+  },
+  {
+    name: '快应用(webview)',
+    value: 'quickapp-webview'
+  },
+  {
+    name: '快应用联盟',
+    value: 'quickapp-webview-union'
+  },
+  {
+    name: '快应用华为',
+    value: 'quickapp-webview-huawei'
+  },
+];
