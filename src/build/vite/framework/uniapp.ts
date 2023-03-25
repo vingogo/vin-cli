543688c1bb46d1a7df5009d6df88076c645a6bf1
Alvin <alvin.lm.liu@gmail.com>
2023-03-25 18:41:11 +0800

    feat: change log language

--- src/build/vite/framework/uniapp.ts
+++ src/build/vite/framework/uniapp.ts
@@ -6,6 +6,9 @@ async function createVite(
   env: EnvType,
   options: Options
 ) {
+  // uniapp 中文日志并不完整，考虑小程序开发主要国内用户，此处默认显示中文
+  process.env.UNI_HBUILDERX_LANGID = 'zh_CN';
+
   if (env === 'development') {
     runDev({ ...options });
   } else {
